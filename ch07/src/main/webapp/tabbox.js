ee236e1973b124e903953c59c448f7fad5e68c51
taesung <ktsung94@naver.com>
2020-04-12 18:23:59 +0900

    image-viewer, tabbox 추가

---
+++ ch07/src/main/webapp/tabbox.js
@@ -0,0 +1,14 @@
+// jQuery 버젼
+var TabBox = {
+	init: function(){
+		$(function() {			
+			$('.tab-view').text($(".tab-box li:last").attr('class', 'selected').text() + "의 탭뷰 입니다.");
+			$(".tab-box li").click(TabBox._onWindowLoad);
+		})
+	},	
+	_onWindowLoad: function(){
+		$('.tab-box li').removeClass('selected');
+		$(this).attr('class', 'selected');
+		$('.tab-view').text($(this).text() + "의 탭뷰 입니다.");
+	}
+}
