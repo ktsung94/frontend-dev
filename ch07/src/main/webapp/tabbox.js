ef3b8f58b7b1a3854fab6969ca341c1a0af8f5ee
taesung <ktsung94@naver.com>
2020-04-13 12:14:52 +0900

    tabbox jquery 버젼

--- ch07/src/main/webapp/tabbox.js
+++ ch07/src/main/webapp/tabbox.js
@@ -1,13 +1,32 @@
-// jQuery 버젼
+//jQuery 버젼
+var TabBox = {
+		init: function(){
+			$(function() {			
+				$('.tab-box div').text($(".tab-box li:last").addClass('selected').text() + "의 탭뷰 입니다.");
+				$(".tab-box li").click(TabBox._onWindowLoad);
+			})
+		},	
+		_onWindowLoad: function(){
+			$('.tab-box li').removeClass('selected');
+			$('.tab-box div').text($(this).addClass('selected').text() + "의 탭뷰 입니다.");
+		}
+}
+
+/*
 var TabBox = {
 	init: function(){
-		$(function() {			
-			$('.tab-view').text($(".tab-box li:last").attr('class', 'selected').text() + "의 탭뷰 입니다.");
-			$(".tab-box li").click(TabBox._onWindowLoad);
-		})
+		$(this._init);		
 	},	
-	_onWindowLoad: function(){
-		$('.tab-box li').removeClass('selected');
-		$('.tab-view').text($(this).attr('class', 'selected').text() + "의 탭뷰 입니다.");
-	}
+	_init: function(){
+		$('.tab-box li').click(TabBox._changeTab);
+		TabBox._changeTab();
+	},
+	_changeTab: function(){
+		$('.tab-box li.selected').removeClass('selected');
+
+		var $liTab = (this == TabBox) ?	$('.tab-box li:first-child') : $(this);
+		$('.tab-box div').text($liTab.data('no') + "의 탭뷰입니다.");
+		$liTab.addClass('selected');
+	}	
 }
+ */
